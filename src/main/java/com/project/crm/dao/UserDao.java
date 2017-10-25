52792274ee52a5fddb4c2157ab40728c32039b84
IvanTkachev <ivan.tkachev1997@yandex.ru>
2017-10-25 15:35:30 +0300

    Add dao package for work with DB

---
+++ src/main/java/com/project/crm/dao/UserDao.java
@@ -0,0 +1,8 @@
+package com.project.crm.dao;
+
+import com.project.crm.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserDao extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
