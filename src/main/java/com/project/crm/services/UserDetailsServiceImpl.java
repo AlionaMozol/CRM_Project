a426093d04531bd07718e1e1e5fb6972a541ea26
IvanTkachev <ivan.tkachev1997@yandex.ru>
2017-10-25 15:44:28 +0300

    Add UserDetailsServiceImpl with method loadUserByUsername()

---
+++ src/main/java/com/project/crm/services/UserDetailsServiceImpl.java
@@ -0,0 +1,46 @@
+package com.project.crm.services;
+
+
+import com.project.crm.dao.UserDao;
+import com.project.crm.model.Role;
+import com.project.crm.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Implementation of {@link UserDetailsService} interface.
+ *
+ * @author Ivan Tkachev
+ * @version 1.0
+ */
+
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        //Проверяем создаваемого пользователя по бд
+        User user = userDao.findByUsername(username);
+        //Созаем множество разрешений
+        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+        //Добавляем в разрешения для данного пользователя все роли, хранящиеся в бд
+        for (Role role : user.getRoles()) {
+            grantedAuthorities.add(new SimpleGrantedAuthority(role.getName()));
+        }
+
+        return new org.springframework.security.core.userdetails.User(user.getUsername(),
+                user.getPassword(), grantedAuthorities);
+    }
+}
