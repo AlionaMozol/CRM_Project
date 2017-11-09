7b8eecf1729fd81ed984bb2bfb2409fba39251e3
ALXSIMINSKI <asiminskiy@mail.ru>
2017-11-09 17:06:52 +0300

    ProductDao, modify SqlService, modify Product

--- src/main/java/com/project/crm/dao/impl/ProductDaoImpl.java
+++ src/main/java/com/project/crm/dao/impl/ProductDaoImpl.java
@@ -11,8 +11,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 
 public class ProductDaoImpl extends DAO implements ProductDao {
@@ -20,9 +19,76 @@ public class ProductDaoImpl extends DAO implements ProductDao {
     @Override
     public void addProduct(Product product) {
         Connection connection = poolInst.getConnection();
+        try {
+           connection.setAutoCommit(false);
+           PreparedStatement statement;
+           ResultSet resultSet;
+           int newObjectTypeId = 0;
+           Map<String, String> attributesAndValues;
 
+           statement = connection.prepareStatement(sql.
+                   getProperty(SqlService.SQL_SELECT_TYPE_ID_BY_CATEGORY_AND_SUPERCATEGORY));
+           statement.setString(1, product.getSuperCategory());
+           statement.setString(2, product.getCategory());
+           resultSet = statement.executeQuery();
 
-        poolInst.footConnection(connection);
+           statement = connection.prepareStatement(sql.
+                   getProperty(SqlService.SQL_INSERT_INTO_OBJECT));
+
+           while(resultSet.next()) {
+              newObjectTypeId = resultSet.getInt(1);
+           }
+           statement.setInt(1, newObjectTypeId);
+           statement.execute();
+
+           attributesAndValues = product.getAttributesAndValues();
+
+           int attributesAttrId = 0;
+           int objectId = 0;
+           for (Map.Entry entry: attributesAndValues.entrySet()) {
+                //Получаем атрибуты и их значения
+                String currentAttribute = (String) entry.getKey();
+                String currentAttributeValue = (String) entry.getValue();
+                //Получаем подходящий attr_id
+                statement = connection.prepareStatement(sql.
+                        getProperty(SqlService.SQL_SELECT_NECESSARY_ATTR_ID));
+                statement.setInt(1, newObjectTypeId);
+                statement.setString(2, currentAttribute);
+                ResultSet attributesAttrIdSet = statement.executeQuery();
+                while(attributesAttrIdSet.next()) {
+                    attributesAttrId = attributesAttrIdSet.getInt(1);
+                }
+                //добавляем запись с продуктом в таблицу
+                statement = connection.prepareStatement(sql.
+                        getProperty(SqlService.SQL_GET_OBJ_ID_BY_OBJ_TYPE_ID));
+                statement.setInt(1, newObjectTypeId);
+                resultSet = statement.executeQuery();
+                while(resultSet.next()) {
+                   objectId = resultSet.getInt(1);
+                }
+
+                statement = connection.prepareStatement(sql.
+                        getProperty(SqlService.SQL_ADD_OBJECT));
+                statement.setInt(1, objectId);
+                statement.setInt(2, attributesAttrId);
+                statement.setString(3, currentAttributeValue);
+                statement.execute();
+           }
+
+           statement.close();
+           resultSet.close();
+           connection.commit();
+        } catch (SQLException e) {
+            try {
+                System.err.print("Transaction is being rolled back");
+                connection.rollback();
+            } catch (SQLException sqlException) {
+                sqlException.printStackTrace();
+            }
+            e.printStackTrace();
+        } finally {
+            poolInst.footConnection(connection);
+        }
     }
 
     @Override
@@ -30,51 +96,60 @@ public class ProductDaoImpl extends DAO implements ProductDao {
         Connection connection = poolInst.getConnection();
         Product currentProduct = new Product();
         try {
-            int i = 0;
-            int g = 0;
+            connection.setAutoCommit(false);
             int currentObjectTypeId = 0;
-            PreparedStatement statement = connection.prepareStatement(sql
+            PreparedStatement statement;
+            ResultSet resultSet;
+            //---------- Получаем Object.Object_type_id
+            statement = connection.prepareStatement(sql
                     .getProperty(SqlService.SQL_GET_PRODUCT_BY_ID));
             statement.setInt(1, id);
-            ResultSet resultSet = statement.executeQuery();
+            resultSet = statement.executeQuery();
             while(resultSet.next()) {
-                currentObjectTypeId = resultSet.getInt(2);
+                currentObjectTypeId = resultSet.getInt(2);                //Object.Object_type_id
             }
-
+            //---------- По Object.Object_type_id получаем категорию и суперкатегорию
             statement = connection.prepareStatement(sql
                     .getProperty(SqlService.SQL_GET_PRODUCT_TYPE_BY_ID));
             statement.setInt(1, currentObjectTypeId);
             resultSet = statement.executeQuery();
             while(resultSet.next()) {
-                /*System.out.println("Record: "
-                        + resultSet.getInt(1) + " "
-                        + resultSet.getString(2) + " "  //Категория
-                        + resultSet.getString(3));      //это суперкатегория - их потом сетаем в currentProduct*/
+                currentProduct.setCategory(resultSet.getString(2));       //Категория
+                currentProduct.setSuperCategory(resultSet.getString(3));  //Cуперкатегория
             }
-            /*statement = connection.prepareStatement(sql.
-                    getProperty(SqlService.SQL_GET_ATTRIBUTE_COST_BY_OBJECT_TYPE_ID));
-            statement.setInt(1, g);
-            resultSet = statement.executeQuery();
-            int h = 0;
-            while(resultSet.next()) {
-                h = resultSet.getInt(1);
-                System.out.println(resultSet.getInt(1)+" - AttrId");
-            }
+            //----------
             statement = connection.prepareStatement(sql.
-                    getProperty(SqlService.SQL_GET_COST_BY_ATTRIBUTE_ID_AND_OBJECT_ID));
-            */
+                    getProperty(SqlService.SQL_GET_CATEGORIES_N_ATTRIBUTES));
+            statement.setInt(1, id);
+            resultSet = statement.executeQuery();
+            Map<String, String> attributesAndValues = new HashMap<>();
+            while(resultSet.next()) {
+               attributesAndValues.put(resultSet.getString(1),
+                                       resultSet.getString(2));
+            }
+            currentProduct.setAttributesAndValues(attributesAndValues);
+            resultSet.close();
+            statement.close();
+            connection.commit();
         } catch (SQLException e) {
+            try {
+                System.err.print("Transaction is being rolled back");
+                connection.rollback();
+            } catch (SQLException sqlException) {
+                sqlException.printStackTrace();
+            }
             e.printStackTrace();
         }
+        finally {
+            poolInst.footConnection(connection);
+        }
 
-        poolInst.footConnection(connection);
-        return null;
+        return currentProduct;
     }
 
     @Override
     public List<Product> getProductsByTitle(String title) {  //Пока нет в базе
         Connection connection = poolInst.getConnection();
-
 
         poolInst.footConnection(connection);
         return null;
@@ -83,16 +158,32 @@ public class ProductDaoImpl extends DAO implements ProductDao {
     @Override
     public List<Product> getProductsByCategory(String category) {
         Connection connection = poolInst.getConnection();
+        List<Product> productsOfTargetCategory = new ArrayList<>();
         try {
+            connection.setAutoCommit(false);
             PreparedStatement statement = connection.prepareStatement(sql
                     .getProperty(SqlService.SQL_GET_PRODUCTS_BY_CATEGORY));
-
-
+            statement.setString(1, category);
+            ResultSet setOfTargetObjectIds = statement.executeQuery();
+            while (setOfTargetObjectIds.next()) {
+                productsOfTargetCategory.add(getProductById(setOfTargetObjectIds.getInt(1)));
+            }
+            setOfTargetObjectIds.close();
+            statement.close();
+            connection.commit();
         } catch (SQLException e) {
+            try {
+                System.err.print("Transaction is being rolled back");
+                connection.rollback();
+            } catch (SQLException sqlException) {
+                sqlException.printStackTrace();
+            }
             e.printStackTrace();
         }
-        poolInst.footConnection(connection);
-        return null;
+        finally {
+            poolInst.footConnection(connection);
+        }
+        return productsOfTargetCategory;
     }
 
     @Override
@@ -104,20 +195,38 @@ public class ProductDaoImpl extends DAO implements ProductDao {
         return null;
     }
 
+    // В БАЗЕ ЦЕНА ХРАНИТСЯ В ВИДЕ СТРОКИ, ПОЭТОМУ, ПОКА ЧТО, ЭТОТ МЕТОД ВОЗВРАЩАЕТ ПУСТОЙ СПИСОК !!!
+    // КОГДА В БАЗЕ ЦЕНА БУДЕТ ХРАНИТЬСЯ В int, ТОГДА ВСЕ ДОЛЖНО РАБОТАТЬ !!!
     @Override
     public List<Product> getProductsBetween2Prices(int priceAfter, int priceBefore) {
         Connection connection = poolInst.getConnection();
+        List<Product> productsOfTargetCost = new ArrayList<>();
         try {
+            connection.setAutoCommit(false);
             PreparedStatement statement = connection.prepareStatement(sql
                     .getProperty(SqlService.SQL_GET_PRODUCTS_BETWEEN_2_PRICES));
-
-
+            statement.setInt(1, priceAfter);
+            statement.setInt(2, priceBefore);
+            ResultSet setOfTargetObjectIds = statement.executeQuery();
+            while (setOfTargetObjectIds.next()) {
+                productsOfTargetCost.add(getProductById(setOfTargetObjectIds.getInt(1)));
+            }
+            setOfTargetObjectIds.close();
+            statement.close();
+            connection.commit();
         } catch (SQLException e) {
+            try {
+                System.err.print("Transaction is being rolled back");
+                connection.rollback();
+            } catch (SQLException sqlException) {
+                sqlException.printStackTrace();
+            }
             e.printStackTrace();
         }
-
-        poolInst.footConnection(connection);
-        return null;
+        finally {
+            poolInst.footConnection(connection);
+        }
+        return productsOfTargetCost;
     }
 
     @Override
@@ -132,32 +241,80 @@ public class ProductDaoImpl extends DAO implements ProductDao {
     @Override
     public List<Product> getAllProducts() {
         Connection connection = poolInst.getConnection();
+        List<Product> allProducts = new ArrayList<>();
         try {
+            connection.setAutoCommit(false);
             PreparedStatement statement = connection.prepareStatement(sql
                     .getProperty(SqlService.SQL_GET_ALL_PRODUCTS));
-
-
+            ResultSet setOfTargetObjectIds = statement.executeQuery();
+            while (setOfTargetObjectIds.next()) {
+                allProducts.add(getProductById(setOfTargetObjectIds.getInt(1)));
+            }
+            setOfTargetObjectIds.close();
+            statement.close();
+            connection.commit();
         } catch (SQLException e) {
+            try {
+                System.err.print("Transaction is being rolled back");
+                connection.rollback();
+            } catch (SQLException sqlException) {
+                sqlException.printStackTrace();
+            }
             e.printStackTrace();
         }
-
-        poolInst.footConnection(connection);
-        return null;
+        finally {
+            poolInst.footConnection(connection);
+        }
+        return allProducts;
     }
 
     @Override
-    public boolean deleteProductById(int id) {
+    public void deleteProductById(int id) {
         Connection connection = poolInst.getConnection();
+        try {
+            connection.setAutoCommit(false);
+            PreparedStatement statement;
+            statement = connection.prepareStatement(sql.
+                    getProperty(SqlService.SQL_DELETE_VALUES_BY_OBJECT_ID));
+            statement.setInt(1, id);
+            statement.execute();
 
+            statement = connection.prepareStatement(sql.
+                    getProperty(SqlService.SQL_DELETE_OBJECT_BY_ID));
+            statement.setInt(1, id);
+            statement.execute();
 
-        poolInst.footConnection(connection);
-        return false;
+            statement.close();
+            connection.commit();
+        } catch (SQLException e) {
+            try {
+                System.err.print("Transaction is being rolled back");
+                connection.rollback();
+            } catch (SQLException sqlException) {
+                sqlException.printStackTrace();
+            }
+            e.printStackTrace();
+        }
+        finally {
+            poolInst.footConnection(connection);
+        }
     }
 
-    /*    public static void main(String[] args) throws ClassNotFoundException {
-          long l = 1;
-          ProductDaoImpl d = new ProductDaoImpl();
-          //Product p = d.getProductById(15);
-        }
-    */
+
+          /*public static void main(String[] args) throws ClassNotFoundException {
+              ProductDaoImpl d = new ProductDaoImpl();
+              Product p = new Product();
+              p.setSuperCategory("Техника");
+              p.setCategory("Телефоны");
+              Map<String, String> map = new HashMap<>();
+              map.put("Состояние","ТЕСТ");
+              map.put("Тип","ТЕСТ");
+              map.put("Марка","ТЕСТ");
+              map.put("Диагональ экрана","ТЕСТ");
+              map.put("Цена","ТЕСТ");
+              map.put("Пользователь","ТЕСТ");
+              p.setAttributesAndValues(map);
+              d.addProduct(p);
+          }*/
+
 }
